121fb2a31dd028ba8e5109fee2695e7d99dba5a4
Heber Marques <heber.f.marques@gmail.com>
2026-06-19 19:09:03 -0300

    introduce IEvent, IHandler and Result pattern

---
+++ src/Modules.Common.Domain/IEvent.cs
@@ -0,0 +1,9 @@
+namespace Modules.Common.Domain;
+
+/// <summary>
+/// Marker interface for domain events.
+/// Events implementing this interface can be published and handled.
+/// </summary>
+public interface IEvent
+{
+}
---
+++ src/Modules.Common.Domain/IHandler.cs
@@ -0,0 +1,9 @@
+namespace Modules.Common.Domain;
+
+/// <summary>
+/// Marker interface for CQRS command/query handlers.
+/// Used for auto-registration of handlers.
+/// </summary>
+public interface IHandler
+{
+}
